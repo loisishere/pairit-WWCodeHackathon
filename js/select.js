c64310be4ae6742a8379da3129535e62cca3bfb4
loisishere <lcl.harris@gmail.com>
2016-07-31 15:20:31 -0400

    Fix else statement; remove array; remove class

--- js/select.js
+++ js/select.js
@@ -24,49 +24,15 @@ $(document).ready(function(){
   }
 } else {
   if($(this).hasClass('active')){
+    var txt = $(this).text();
     for(var i=0;i<skillsArr.length;i++){
       if(skillsArr[i] === txt){
         skillsArr.splice(i,1);
       }
   }
+  $(this).removeClass("active");
 }
-//     }else{
-//       if($(this).css("color")==="rgb(9, 93, 155)"){
-//     var txt = $(this).text();
-//     for(var i=0;i<skillsArr.length;i++){
-//       if(skillsArr[i] === txt){
-//         skillsArr.splice(i,1);
-//       }
-//     }
-//     $(this).css("color","#343436");
-//     }
-//     }
-// })
-//   $(".interests").click(function(){
-//     if(interestArr.length <interests){
-//       if($(this).css("color")==="rgb(52, 52, 54)"){
-//     interestArr.push($(this).text());
-//     $(this).css("color","#095d9b");
-//     }
-//   else if($(this).css("color")==="rgb(9, 93, 155)"){
-//     var txt = $(this).text();
-//     for(var i=0;i<interestArr.length;i++){
-//       if(interestArr[i] === txt){
-//         interestArr.splice(i,1);
-//       }
-//     }
-//     $(this).css("color","#343436");
-//   }
-//     }else{
-//       if($(this).css("color")==="rgb(9, 93, 155)"){
-//     var txt = $(this).text();
-//     for(var i=0;i<interestArr.length;i++){
-//       if(interestArr[i] === txt){
-//         interestArr.splice(i,1);
-//       }
-//     }
-//     $(this).css("color","#343436");
-//   }
-//     }
+}
+
   });
 });
